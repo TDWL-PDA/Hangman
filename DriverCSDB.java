cc60f19ad23b1a870eb5443ace1722dc3d569e96
Dziugas Butkus <dziubutkus@gmail.com>
2018-04-26 15:24:36 -0600

    Almost finished

--- DriverCSDB.java
+++ DriverCSDB.java
@@ -10,9 +10,11 @@ public class DriverCSDB {
 	public static java.io.File inFile;  //input file
 	public static Scanner inputFile;    //used to read lines of input file
 	public static BufferedWriter beforeFile;	// out file for tree before deleting words.
+	public static Scanner input;
 	
 	public static void main(String[] args) throws IOException
 	{
+		input = new Scanner(System.in);
 		//Open File
 		inFile = new java.io.File("TitlesandAuthors.csv");  //input file
 		//inFile = new java.io.File("bookTitles.txt");
@@ -54,10 +56,131 @@ public class DriverCSDB {
 			}
 		}
 		
-		bookTitles.deleteNode("zorba the greek");
-		bookTitles.printInOrder();
-		System.out.println("\nThere are " + size + " book titles");
-	  
+		//bookTitles.deleteNode("zorba the greek");
+		//bookTitles.printInOrder();
+		//System.out.println("\nThere are " + size + " book titles");
+		
+		// Find the node
+		String word = bookTitles.getNode().getWord();
+		String hint = bookTitles.getNode().getHint();
+		System.out.println(word + " " + hint);
+		
+		// Get spaces
+		char[] guessArray = new char[word.length()];
+		int underscores = word.length();
+		
+		getWord(word, guessArray, underscores); // Put '_' and ' ' into char array 
+		//printGuess(guessArray, underscores); // Print guessArray[]
+		
+		int mistakes = 0;
+		
+		String[] hangedMan = new String[7];
+		hangedMan[0] = "    O\n";
+		hangedMan[1] = "   /";
+		hangedMan[2] = "|";
+		hangedMan[3] = "\\ \n";
+		hangedMan[4] = "    |\n";
+		hangedMan[5] = "   /";
+		hangedMan[6] = " \\ \n";
+		
+		boolean correct = false;
+		char guess;
+		
+		while(correct != true && mistakes != 7)
+		{
+			hangAMan(hangedMan, mistakes);
+			printGuess(guessArray, underscores);
+			System.out.print("Input your guess: ");
+			guess = input.next().toLowerCase().charAt(0);
+			mistakes = checkGuess(guess, underscores, word, guessArray, mistakes);
+			correct = checkGameWon(correct, underscores, guessArray, mistakes, word);
+		}
+	}
+	
+	public static boolean checkGameWon(boolean correct, int underscores, char[] guessArray, int mistakes, String word)
+	{
+		int temp = 0;
+		if(mistakes == 7)
+		{
+			System.out.println("You made 7 mistakes and lost the game.");
+		}
+		else
+		{
+			for(int i = 0; i < underscores; i++)
+			{
+				if(guessArray[i] != '_' && guessArray[i] != ' ')
+				{
+					temp++;
+				}
+			}
+		}
+		if(temp == guessArray.length)
+		{
+			correct = false;
+			System.out.println("You guessed the word right!");
+
+		}
+
+		return correct;
+	}
+	
+	public static int checkGuess(char guess, int underscores, String word, char[] guessArray, int mistakes)
+	{
+		int isChar = -1;
+		isChar = word.indexOf(guess);
+		if(isChar != -1)
+		{
+			for(int i = 0; i < underscores; i++)
+			{
+				if(word.charAt(i) == guess)
+				{
+					guessArray[i] = guess;
+				}
+			}			
+		}
+		else
+		{
+			mistakes++;
+		}
+		return mistakes;
+	}
+	
+	public static void hangAMan(String[] hangedMan, int mistakes)
+	{
+		// Always print
+		System.out.println("    _____");
+		System.out.println("    |");
+		System.out.println("    |");
+		// Print according to mistakes
+		for(int i = 0; i < mistakes; i++)
+		{
+			System.out.print(hangedMan[i]);
+		}
+		System.out.println();
+	}
+	
+	public static void printGuess(char[] guessArray, int underscores)
+	{
+		for(int i = 0; i < underscores; i++)
+		{
+			System.out.print(guessArray[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	public static void getWord(String word, char[] guessArray, int underscores)
+	{
+		for(int i = 0; i < underscores; i++)
+		{
+			if(word.charAt(i) != (' '))
+			{
+				guessArray[i] = '_';
+			}
+			else
+			{
+				guessArray[i] = ' ';
+			}
+		}
 	}
 
 }
